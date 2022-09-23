dc7faacfb06f1f6be64c7b46d6a8d2cebafc769a
Jackie Ma <alerk_star@outlook.com>
2022-09-24 00:34:43 +0800

    configured routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/Home';
+import EventPage from './pages/Event';
+
+const routers = [
+  {
+    path: '/',
+    component: <HomePage />
+  },
+  {
+    path: '/event',
+    component: <EventPage />
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+      {
+        routers.map(router => 
+        <Route 
+          key={router.path}
+          path={router.path}
+          element={router.component}
+        />)
+      }
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Event.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EventPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Event Page</h1>
+      <p>Content goes here</p>
+    </div>
+  )
+}
+
+export default EventPage;
